6570df45430fd84611a310517cffc30dd5b25ac1
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2020-06-28 21:11:26 -0600

    put chalice code all in app.py

--- {{cookiecutter.project}}/app.py
+++ {{cookiecutter.project}}/app.py
@@ -1,1 +1,11 @@
-from {{cookiecutter.project|replace("-", "_")}}.chalice import app
+import chalice
+
+
+app = chalice.Chalice(app_name="{{cookiecutter.project}}")
+
+
+@app.route("/")
+def index():
+    """This is just an initial test endpoint."""
+    # TODO: delete this method once finished testing
+    return {"hello": "{{cookiecutter.project}}"}
